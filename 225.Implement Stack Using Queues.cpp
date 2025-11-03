7ce0ef04e337f6b14166138deee1bd011679f329
Edko21 <146870569+Edko21@users.noreply.github.com>
2025-11-03 17:34:43 +0200

    Add files via upload

---
+++ 225.Implement Stack Using Queues.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+	queue<int> q1;
+	queue<int> q2;
+public:
+	MyStack(){}
+	
+	void push(int x) {
+		q2.push(x);
+		while (!q1.empty()) {
+			q2.push(q1.front());
+			q1.pop();
+		}
+		swap(q1, q2);
+	}
+
+	int pop() {
+		int topEl = q1.front();
+		q1.pop();
+		return topEl;
+	}
+
+	int top() {
+		return q1.front();
+	}
+
+	bool empty() {
+		return q1.empty();
+	}
+};

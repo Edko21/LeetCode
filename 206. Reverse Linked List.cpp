50be4448318d3a04b92100862f9446a445ce04d3
Edko21 <146870569+Edko21@users.noreply.github.com>
2025-11-07 11:31:17 +0200

    Add files via upload

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,30 @@
+/**
+  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ **/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head)
+            return head;
+
+        ListNode* prev = 0;
+        ListNode* next = head->next;
+        ListNode* curr = head;
+
+        while (next) {
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            next = curr->next;
+        }
+        curr->next = prev;
+        return curr;
+    }
+};
